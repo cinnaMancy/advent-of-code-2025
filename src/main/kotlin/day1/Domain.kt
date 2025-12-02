bc4946056c3693b0f56430d0915891740654857c
cinnaMancy <cinnamancy@gmail.com>
2025-12-02 07:37:22 +0100

    Day1 refac

--- src/main/kotlin/day1/Domain.kt
+++ src/main/kotlin/day1/Domain.kt
@@ -4,32 +4,23 @@ class RotationSequence(
     private val rotations: List<Rotation>
 ) {
     fun endClicks(): Int = rotations
-        .runningFold(50) { position, rotation -> rotate(position, rotation) }
-        .count { it == 0 }
+        .runningFold(50L) { position, rotation -> rotate(position, rotation) }
+        .count { it % 100 == 0L }
 
     fun allClicks(): Int = rotations
-        .fold(Pair(50, 0)) { positionToAllClicks, rotation ->
-            Pair(
-                rotate(positionToAllClicks.first, rotation),
-                positionToAllClicks.second + passesZeroAmount(positionToAllClicks.first, rotation)
-            )
-        }.second
+        .runningFold(50L) { position, rotation -> rotate(position, rotation) }
+        .zipWithNext()
+        .sumOf { clicksAmount(it.first, it.second) }
 
-    private fun rotate(position: Int, rotation: Rotation): Int {
-        var newPosition = (position + rotation.displacement) % 100
-        if (newPosition < 0) newPosition += 100
-        return newPosition
-    }
+    private fun rotate(position: Long, rotation: Rotation): Long =
+        position + rotation.displacement
 
-    private fun passesZeroAmount(position: Int, rotation: Rotation): Int {
-        val unclippedNewPosition = position + rotation.displacement
-        return if (unclippedNewPosition > 0) {
-            unclippedNewPosition / 100
-        } else {
-            val firstPassover = if (position == 0) 0 else 1
-            (-unclippedNewPosition / 100) + firstPassover
-        }
-    }
+    private fun clicksAmount(firstPosition: Long, secondPosition: Long): Int =
+        if (firstPosition >= secondPosition) clicksGoingUpZeroAmount(firstPosition, secondPosition)
+        else clicksGoingUpZeroAmount(-firstPosition, -secondPosition)
+
+    private fun clicksGoingUpZeroAmount(firstPosition: Long, secondPosition: Long): Int =
+        (firstPosition.floorDiv(100) - secondPosition.floorDiv(100)).toInt()
 
     companion object {
         fun parse(lines: List<String>) = RotationSequence(lines.map(Rotation.Companion::parse))
@@ -38,20 +29,20 @@ class RotationSequence(
 
 class Rotation(
     private val direction: Direction,
-    private val distance: Int
+    private val distance: Long
 ) {
-    val displacement get(): Int = direction.distanceMultiplier * distance
+    val displacement get(): Long = direction.distanceMultiplier * distance
 
     companion object {
         fun parse(line: String) = Rotation(
             Direction.parse(line.first()),
-            line.substring(1).toInt()
+            line.substring(1).toLong()
         )
     }
 }
 
 enum class Direction(
-    val distanceMultiplier: Int
+    val distanceMultiplier: Long
 ) {
     LEFT(-1),
     RIGHT(1);
